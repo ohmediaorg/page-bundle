8ffea84584193b0a9d13ee663b9f4d583f354230
Justin Hoffman <justin@ohmedia.ca>
2024-05-01 16:25:21 -0600

    remove unneeded function call

--- src/OHMediaPageBundle.php
+++ src/OHMediaPageBundle.php
@@ -12,8 +12,6 @@ class OHMediaPageBundle extends AbstractBundle
 {
     public function build(ContainerBuilder $container): void
     {
-        parent::build($container);
-
         $container->addCompilerPass(new PagePass());
     }
 
