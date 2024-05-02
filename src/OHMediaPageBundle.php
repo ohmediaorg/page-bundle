9c7cd63c0ba0292aefa365c9b608c90ce8bb3324
Justin Hoffman <justin@ohmedia.ca>
2024-05-02 09:51:18 -0600

    add back function call for future-proofing

--- src/OHMediaPageBundle.php
+++ src/OHMediaPageBundle.php
@@ -12,6 +12,8 @@ class OHMediaPageBundle extends AbstractBundle
 {
     public function build(ContainerBuilder $container): void
     {
+        parent::build($container);
+
         $container->addCompilerPass(new PagePass());
     }
 
