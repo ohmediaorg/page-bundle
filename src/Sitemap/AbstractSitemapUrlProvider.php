bc62a8dc258fb3c26f744743cc081a3e9817b0e9
Justin Hoffman <justin@ohmedia.ca>
2024-07-31 15:17:52 -0600

    mock up AbstractSitemapUrlProvider class

---
+++ src/Sitemap/AbstractSitemapUrlProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OHMedia\PageBundle\Sitemap;
+
+abstract class AbstractSitemapUrlProvider
+{
+    private array $sitemapUrls = [];
+
+    abstract protected function buildSitemapUrls(): void;
+
+    final protected function addSitemapUrl(string $path, \DateTimeInterface $lastmod): static
+    {
+        $this->sitemapUrls = new SitemapUrl($path, $lastmod);
+
+        return $this;
+    }
+
+    final public function getSitemapUrls(): array
+    {
+        $this->buildSitemapUrls();
+
+        return $this->sitemapUrls;
+    }
+}
