cd1192d8c548a11cba393f391b8af01b712f3ec8
Justin Hoffman <justin@ohmedia.ca>
2024-08-16 13:54:38 -0600

    cache the homepage query result

--- src/Repository/PageRepository.php
+++ src/Repository/PageRepository.php
@@ -16,6 +16,8 @@ use OHMedia\PageBundle\Entity\Page;
  */
 class PageRepository extends ServiceEntityRepository
 {
+    private ?Page $homepage = null;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Page::class);
@@ -72,10 +74,14 @@ class PageRepository extends ServiceEntityRepository
 
     public function getHomepage(): ?Page
     {
-        return $this->createQueryBuilder('p')
-            ->where('p.homepage = 1')
-            ->setMaxResults(1)
-            ->getQuery()
-            ->getOneOrNullResult();
+        if (!$this->homepage) {
+            $this->homepage = $this->createQueryBuilder('p')
+                  ->where('p.homepage = 1')
+                  ->setMaxResults(1)
+                  ->getQuery()
+                  ->getOneOrNullResult();
+        }
+
+        return $this->homepage;
     }
 }
