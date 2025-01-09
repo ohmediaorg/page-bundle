acb55ebf23463c63de624853930f211b7e6c55b6
Justin Hoffman <justin@ohmedia.ca>
2025-01-09 16:12:34 -0600

    only allow developer user to select dynamic templates

--- src/Form/PageRevisionType.php
+++ src/Form/PageRevisionType.php
@@ -3,7 +3,10 @@
 namespace OHMedia\PageBundle\Form;
 
 use OHMedia\PageBundle\Entity\PageRevision;
+use OHMedia\PageBundle\Form\Type\AbstractDynamicPageTemplateType;
+use OHMedia\PageBundle\Form\Type\AbstractPageTemplateType;
 use OHMedia\PageBundle\Service\PageManager;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,22 +14,49 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PageRevisionType extends AbstractType
 {
-    public function __construct(private PageManager $pageManager)
-    {
+    public function __construct(
+        private PageManager $pageManager,
+        private Security $security,
+    ) {
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $page = $options['data'];
 
-        $templates = [];
-
         $pageTemplateTypes = $this->pageManager->getPageTemplateTypes();
 
-        foreach ($pageTemplateTypes as $pageTemplateType) {
-            $label = call_user_func([$pageTemplateType, 'getTemplateName']);
+        // pre-order things for the selection
+        usort($pageTemplateTypes, function (AbstractPageTemplateType $a, AbstractPageTemplateType $b) {
+            $aLabel = call_user_func([$a, 'getTemplateName']);
+            $bLabel = call_user_func([$b, 'getTemplateName']);
 
-            $templates[$label] = $pageTemplateType::class;
+            return $a <=> $b;
+        });
+
+        // distinguish between dynamic and non-dynamic
+        $dynamicTemplates = [];
+        $templates = [];
+
+        foreach ($pageTemplateTypes as $pageTemplateType) {
+            $isDynamic = $pageTemplateType instanceof AbstractDynamicPageTemplateType;
+
+            $label = call_user_func([$pageTemplateType, 'getTemplateName']);
+            $value = $pageTemplateType::class;
+
+            if ($isDynamic) {
+                $dynamicTemplates[$label] = $value;
+            } else {
+                $templates[$label] = $value;
+            }
+        }
+
+        $user = $this->security->getUser();
+
+        if ($user->isTypeDeveloper()) {
+            // dynamic templates are only available to a developer user
+            // append them to the selection
+            $templates = array_merge($templates, $dynamicTemplates);
         }
 
         $builder->add('template', ChoiceType::class, [
